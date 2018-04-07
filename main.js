fcc3db21777c273c66bfad40c386256db13162cf
Daniel Crewdson <danielcrewdson@gmail.com>
2018-04-07 21:19:26 +0100

    about me button and info added

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-jQuery('.fadeIn').addClass("hidden").viewportChecke({
-    classToAdd: 'visible animated fadeIn',
-    offset: 100
-});
+function showAbout() {
+    var aboutText = document.getElementById("about-wrap");
+    if(aboutText.style.display === "none") {
+        aboutText.style.display = "block";
+    } else {
+        aboutText.style.display = "none";
+    }
+};
